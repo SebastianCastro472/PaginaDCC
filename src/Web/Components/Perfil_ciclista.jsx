6cf6376e75a25b087383ecd0417ce5c24e7ad77e
SebastianCastro472 <sebacastro472@gmail.com>
2024-06-02 17:24:56 -0300

    seguimos con Perfil ciclista

--- src/Web/Components/Perfil_ciclista.jsx
+++ src/Web/Components/Perfil_ciclista.jsx
@@ -3,6 +3,7 @@ const Redes1 = new URL('../../imagenes/instagram.png', import.meta.url).href;
 const Redes2 = new URL('../../imagenes/facebook.png', import.meta.url).href;
 const NACIONALIDAD = new URL('../../imagenes/uruguay.png', import.meta.url).href;
 const Foto = new URL('../../imagenes/Foto2.jpg', import.meta.url).href;
+const tato = new URL('../../imagenes/Ciclistas/SinFondo/Tato-sf.png', import.meta.url).href;
 
 const Team = () => {
     return (
@@ -10,8 +11,10 @@ const Team = () => {
         <div className="Perfil-SolidoFoto">
             <div className="Perfil-Foto">
                 <img src={Foto} alt="" />
+                <h2>Stefano</h2>
+                <h1>Uran</h1>
             </div>
-            <div className="Perfil-Solido" style={{ backgroundColor: 'green' }}>
+            <div className="Perfil-Solido" >
                 
             </div>
         </div>
@@ -20,7 +23,7 @@ const Team = () => {
                 <h2>info</h2>
             </div>
             <div className="Perfil-Espacio">
-                <h2>Perfil</h2>
+                <img src={tato} alt="" />
             </div>
         </div>
     </div>
